d0500e3b8dd6bd02b04b75a232ee83879b25b24f
Aditya Godambe <godambeaditya@gmail.com>
2025-07-25 20:05:25 +0530

    FEAT: Cleaned linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,11 +3,13 @@ from psycopg2 import OperationalError as Pycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database....')
         db_up = False
+
         while db_up is False:
             try:
                 self.check(databases=['default'])
@@ -15,5 +17,7 @@ class Command(BaseCommand):
             except (Pycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second ...')
                 time.sleep(1)
-        
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+
+        self.stdout.write(
+            self.style.SUCCESS('Database available!')
+        )
