2b56774beed848a52777bed7225eef2e5bcf2ae1
Aditya Godambe <godambeaditya@gmail.com>
2025-07-25 19:49:19 +0530

    FEAT: Implemented wait_for_db

---
+++ app/app/test.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_two_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_two_numbers(self):
+        res = calc.subtract(10,5)
+
+        self.assertEqual(res,5)
